a2f0c25724d2fabbb2dbb27d9281798f08cc067b
shana25 <74354030+shana25@users.noreply.github.com>
2020-11-26 15:47:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup()
+{
+  createCanvas(800,400);
+  fixedrect=createSprite(400, 200, 60, 100);
+  movingrect=createSprite(600, 200, 100, 60);
+  fixedrect.shapeColor="green"
+  movingrect.shapeColor="green"
+}
+
+function draw() {
+  background(0); 
+  movingrect.x=World.mouseX 
+  movingrect.y=World.mouseY
+  
+  if (movingrect.x-fixedrect.x<movingrect.width/2+fixedrect.width/2 &&
+       fixedrect.x-movingrect.x<movingrect.width/2+fixedrect.width/2 &&
+       movingrect.y-fixedrect.y<movingrect.height/2+fixedrect.height/2 &&
+       fixedrect.y-movingrect.y<movingrect.height/2+fixedrect.height/2)
+  {
+    fixedrect.shapeColor="red"
+    movingrect.shapeColor="red"
+  }
+  else
+  {
+    fixedrect.shapeColor="green"
+    movingrect.shapeColor="green"
+  }
+
+  drawSprites();
+}
